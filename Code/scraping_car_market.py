2301024a4bc84e4814204c8b390c8f5600b2db00
Martin Wachter <martinwachter@gmx.net>
2019-07-28 21:29:48 +0200

    Improved handling of changing web data and fixed a data type issue in plotting function

--- Code/scraping_car_market.py
+++ Code/scraping_car_market.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
-import matplotlib.pyplot as  plt
+import matplotlib.pyplot as plt
+import numpy as np
 import csv
+import os
 import logging
 
 
@@ -18,10 +20,10 @@ def car_details_to_dict(price='', info_string=''):
             'fuel': 'petrol',
             'fuel_consumption_per_100_km': 0}
         list_of_details = info_string.splitlines()
-        car_detail_data_set['mileage'] = int(list_of_details[0].split(' ')[0].replace('.', ''))
-        car_detail_data_set['construction_year'] = int(list_of_details[1].split('/')[1])
+        car_detail_data_set['mileage'] = handle_dash_in_text(list_of_details[0])
+        #car_detail_data_set['construction_year'] = int(list_of_details[1].split('/')[1])
         car_detail_data_set['power'] = int(list_of_details[2].split('(')[1].split(' ')[0])
-        car_detail_data_set['car_owner'] = int(list_of_details[4].split(' ')[0])
+        #car_detail_data_set['car_owner'] = int(list_of_details[4].split(' ')[0])
         car_detail_data_set['transmission'] = list_of_details[5]
         car_detail_data_set['fuel'] = list_of_details[6]
         car_detail_data_set['fuel_consumption_per_100_km'] = list_of_details[7].split(' ')[0]
@@ -33,20 +35,40 @@ def car_details_to_dict(price='', info_string=''):
         return print('Improper info string')
 
 
+def handle_dash_in_text(text = ''):
+    filtered_text = text.split(' ')[0]
+    if filtered_text == '-':
+        return '0'
+    if filtered_text.find('.') != -1:
+        return int(filtered_text.replace('.', ''))
+    else:
+        return int(filtered_text)
+
+
 def plotting_car_data(car_data_list = [{}]):
-    mileage_list = []
-    price_list = []
+    x = np.array([], dtype=int)
+    y = np.array([], dtype=int)
+
     for car_data in car_data_list:
-        mileage_list.append(car_data.get('mileage'))
-        price_list.append(car_data.get('price'))
+        x = np.append(x, [int(car_data.get('mileage'))])
+        y = np.append(y, [int(car_data.get('price'))])
+
+    #initialize plot
+    plt.suptitle('Car Value Plot')
     plt.xlabel('Mileage in km')
-    plt.ylabel('Price in Euro')
-    plt.scatter(mileage_list, price_list)
+    plt.ylabel('Price in €')
+    plt.grid(True)
+    plt.plot(x, y, 'bx')
+
+    #set axis
+    #plt.axis([np.amin(x), np.amax(x), np.amin(y), np.amax(y)])
+
+    #show plot
     plt.show()
 
 
-def writing_to_car_market_csv(car_data_list = [{}]):
-    with open('Data/car_market_data.csv', 'w') as csv_file:
+def writing_to_car_market_csv(path, car_data_list = [{}]):
+    with open(os.path.join(path, 'car_market_data.csv'), 'w') as csv_file:
         fieldnames = ['mileage', 'price', 'construction_year', 'power', 'car_state', 'car_owner', 'transmission', 'fuel', 'fuel_consumption_per_100_km']
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter='\t')
         csv_writer.writeheader()
@@ -67,7 +89,7 @@ delta_km = 10000
 driver = webdriver.Firefox()
 dict_details = []
 
-url = 'https://www.autoscout24.de/lst/mercedes-benz/cla-(alle)?sort=standard&desc=0&fuel=B&ustate=N%2CU&powerfrom=147&powertype=hp&cy=D&kmto=10000&fregfrom=2019&atype=C'
+url = 'https://www.autoscout24.de/lst/land-rover/range-rover-evoque?sort=standard&desc=0&eq=140%2C155%2C23&gear=A&fuel=B&doorfrom=4&doorto=5&ustate=N%2CU&page=1&powerfrom=147&powertype=hp&cy=D&kmto=10000&ptype=M&atype=C'
 
 
 for page in range(1, num_max_pages+1):
@@ -88,6 +110,6 @@ for page in range(1, num_max_pages+1):
 
 driver.close() #close web browser
 
-writing_to_car_market_csv(dict_details)
+writing_to_car_market_csv('/Users/Martin/GitKraken/CarMarketCrawling/Data', dict_details)
 
 plotting_car_data(dict_details)
--- Code/test.py
+++ Code/test.py
@@ -1,26 +1,12 @@
-import csv
-import matplotlib.pyplot as plt
-import numpy as np
+string = '900 km'
 
-def read_car_market_csv(path):
-    car_market_data = []
-    with open(path, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter='\t')
-        for line in csv_reader:
-            car_market_data.append(line)
-        return car_market_data
+def handle_dash_in_text(text = ''):
+    filtered_text = text.split(' ')[0]
+    if filtered_text == '-':
+        return '0'
+    if filtered_text.find('.') != -1:
+        return int(filtered_text.replace('.', ''))
+    else:
+        return int(filtered_text)
 
-def plotting_car_data(car_data_list = [{}]):
-    x = []
-    y = []
-    for car_data in car_data_list:
-        x.append(car_data.get('mileage'))
-        y.append(car_data.get('price'))
-    plt.xlabel('Mileage in km')
-    plt.ylabel('Price in Euro')
-    plt.plot(x, y, 'ro')
-    plt.show()
-
-car_data = read_car_market_csv('/Users/Martin/GitKraken/CarMarketCrawling/Data/car_market_data.csv')
-print(car_data)
-plotting_car_data(car_data)
+print(handle_dash_in_text(string))
