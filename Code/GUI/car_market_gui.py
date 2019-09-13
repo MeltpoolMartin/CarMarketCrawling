99587079e75e090b05d4c0e795efdc5a690e95a6
Martin Wachter <martinwachter@gmx.net>
2019-09-13 23:12:22 +0200

    Able to load dummy data into GUI canvas --> preparing web crawling and reading csv files

--- Code/GUI/car_market_gui.py
+++ Code/GUI/car_market_gui.py
@@ -1,8 +1,11 @@
+from selenium import webdriver
 from PyQt5.uic import loadUiType
 import sys, csv
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QSizePolicy, QWidget
 from PyQt5.QtWidgets import QFileDialog
 import numpy as np
+from numpy import arange, sin, pi
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (FigureCanvasQTAgg as FigureCanvas,
                                                 NavigationToolbar2QT as NavigationToolbar)
@@ -11,47 +14,65 @@ from matplotlib.backends.backend_qt5agg import (FigureCanvasQTAgg as FigureCanva
 UI_main_window, QMain_Window = loadUiType('gui.ui')
 
 
-class CarMarketCarwlingUI(QMain_Window, UI_main_window):
-    def __init__(self, ):
-        super(CarMarketCarwlingUI, self).__init__()
+class CreateCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+
+        self.compute_initial_figure()
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+
+class PlottingCanvas(CreateCanvas):
+    def compute_initial_figure(self):
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2*pi*t)
+        self.axes.plot(t, s)
+
+
+class CarMarketCrawling():
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.url = ""
+
+    def crawl_website(self, url):
+        xpath_price = '//span[@class="cldt-price sc-font-xl sc-font-bold"]'
+        xpath_detail = '//ul[@data-item-name="vehicle-details"]'
+        xpath_filter = '//span[@class="sc-tag__label"]'
+        self.driver.get(url)
+        self.detail = self.dirver.find_elements_by_xpath(xpath_price)
+        self.detail = self.dirver.find_elements_by_xpath(xpath_detail)
+        self.filter = self.dirver.find_elements_by_xpath(xpath_filter)
+        self.driver.close()
+
+
+class CarMarketCrawlingUI(QMain_Window, UI_main_window):
+    def __init__(self):
+        super(CarMarketCrawlingUI, self).__init__()
         self.setupUi(self)
-        self.csv_reader = []
+        self.ui = QWidget(self)
 
-        self.menuOpen.addAction(self.actionOpen)
-        self.actionOpen.triggered.connect(self.open_file_name_dialog)
+        sc = PlottingCanvas(self.ui  , width=5, height=4, dpi=100)
+        self.mplvl.addWidget(sc)
 
-    def open_file_name_dialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        file_path, file_type = QFileDialog.getOpenFileName(self, caption="Choose a *.csv file",
-                                                           filter='*.csv', options=options)
-        print(file_path)
-        self.read_car_market_csv(file_path)
-
-    def read_car_market_csv(self, path):
-        with open(path, 'r') as csv_file:
-            self.csv_reader = csv.DictReader(csv_file, delimiter='\t')
-            for line in self.csv_reader:
-                print(line)
-
-    # def add_fig(self, name, fig):
-    #     print(name, fig.text)
-    #     self.fig_dict[name] = fig
-    #     self.mplFigs.addItem(name)
-    #
-    # def add_plot(self):
-    #     self.canvas = FigureCanvas(fig)
-    #     self.mplvl.addWidget(self.canvas)
-    #     self.canvas.draw()
-    #     self.toolbar = NavigationToolbar(self.canvas,
-    #                                      self, coordinates=True)
-    #     self.addToolBar(self.toolbar)
+        # self.menuOpen.addAction(self.actionOpen)
+        # self.actionOpen.triggered.connect(self.load_car_market_data)
 
 
 if __name__ == '__main__':
 
     app = QtWidgets.QApplication(sys.argv)
-    main = CarMarketCarwlingUI()
+    main = CarMarketCrawlingUI()
 
     main.show()
     sys.exit(app.exec_())
